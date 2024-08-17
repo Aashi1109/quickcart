75f6a0135b2b0941decda973ce8ecbc5e5da78b0
Ashish Pal <aashishpal50@gmail.com>
2024-08-17 19:35:09 +0530

    ENV added for DB url

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@ const config = {
   productsAPIUrl: process.env.NEXT_PUBLIC_PRODUCTS_API_URL,
   db: {
     name: "QuickCart",
-    url: "mongodb://localhost:27017/quickcart",
+    url: process.env.MONGODB_URL || "",
   },
   nextAuth: {
     google: {
